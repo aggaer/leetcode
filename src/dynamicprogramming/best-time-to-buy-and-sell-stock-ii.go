474f22b0f83e856b7a3bf83aa97823dfcf2f7dc2
Jerry <yuanxing@ipaynow.cn>
2019-07-22 23:47:30 +0800

    add dynamicprogramming

---
+++ src/dynamicprogramming/best-time-to-buy-and-sell-stock-ii.go
@@ -0,0 +1,11 @@
+package dynamicprogramming
+
+func MaxProfit2(prices []int) int {
+	maxProfit := 0
+	for i := 1; i < len(prices); i++ {
+		if prices[i] > prices[i-1] {
+			maxProfit += prices[i] - prices[i-1]
+		}
+	}
+	return maxProfit
+}
