ca80a5796bad0aae32a116aea1ff79ff310bcb71
Jerry <yuanxing@ipaynow.cn>
2019-08-14 22:26:58 +0800

    add count-primes

---
+++ src/others/count-primes.go
@@ -0,0 +1,16 @@
+package others
+
+func countPrimes(n int) int {
+	a := make([]bool, n)
+	cnt := 0
+	for i:=2; i<n; i++ {
+		if a[i] {                  
+			continue
+		}
+		for j:=i+i; j<n; j+=i {
+			a[j] = true
+		}
+		cnt++
+	}
+	return cnt
+}
