4e0e7842125f4d7aff0c3798502371f4867676a2
Jerry <yuanxing@ipaynow.cn>
2019-09-06 13:56:55 +0800

    add servals

--- src/main/testcase.go
+++ src/main/testcase.go
@@ -1,9 +1,10 @@
 package main
 
-import "leetcode/src/str"
+import "leetcode/src/arr"
 
 //"---"
 //3
 func main() {
-	println(str.LicenseKeyFormatting("---", 3))
+	//println(str.LicenseKeyFormatting("---", 3))
+	println(arr.FindMaxConsecutiveOnes([]int{1,0,1,1}))
 }
